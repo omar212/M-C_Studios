4069dacb60c926bbaa34c901ac957e7251058e58
Omar <omarelnagdy16@gmail.com>
2018-10-30 17:59:06 -0400

    deleted useless stuff

--- mcstudios/src/App.js
+++ mcstudios/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import header from './Components/Header';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyDVL3Lvrvc0E4i6CiuY3Ay9wkjvZona4y0",
+//   authDomain: "login-form-try-2.firebaseapp.com"
+// })
 
 class App extends Component {
+  // state = { isSignedIn: false }
+  // uiConfig = {
+  //   signInFlow: "popup",
+  //   signInOptions: [
+  //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  //     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  //     firebase.auth.EmailAuthProvider.PROVIDER_ID
+  //   ],
+  //   callbacks: {
+  //     signInSuccess: () => false
+  //   }
+  // }
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <div>
+        <header />
+        <h1> Welcome To The Movie Page</h1>
+       </div>
+
     );
   }
 }
